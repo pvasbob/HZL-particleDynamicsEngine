2f118bc7e4ae9722594b3b43cd0f6d748d22c6ad
pvasbob <pvasbob@gmail.com>
2026-07-10 22:24:30 -0400

    initialize a window, link to opengl lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,52 @@
+#include <GLFW/glfw3.h>
+
+#include <cstdlib>
+
 #include <iostream>
+
 
 int main() 
 {
-    std::cout << "HZL Particle Dynamcics Engine: startup successful. \n";
-    return 0;
+    if (glfwInit() == GLFW_FALSE)
+    {
+        std::cerr << "Failed to initialize GLFW. \n";
+        return EXIT_FAILURE; 
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(
+        1280,
+        720,
+        "HZL Particle Dynamics Engine",
+        nullptr,
+        nullptr
+    );
+
+    if (window == nullptr)
+    {
+        std::cerr << "Failed to create GLFW window. \n";
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    while(glfwWindowShouldClose(window) == GLFW_FALSE)
+    {
+        glfwPollEvents();
+
+        glClearColor(0.04f, 0.07f, 0.12f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
 }
